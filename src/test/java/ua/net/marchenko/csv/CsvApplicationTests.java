629ea07e4e4c4be1fa45a3cfcfde5f48623439ad
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2021-06-26 12:40:26 +0300

    initial

---
+++ src/test/java/ua/net/marchenko/csv/CsvApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.net.marchenko.csv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
